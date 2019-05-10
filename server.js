c589cac8533b7f909bdafa5a10bd01c95daed417
Emily Barcroft <emily.barcroft@metaswitch.com>
2019-05-10 13:23:46 +0100

    resetting wall stuff

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ setInterval(function(){
       let oldY = state.y;
 
       if (connected[key].left === 1) {
-
         state.x -= SPEED;
       }
 
@@ -170,22 +169,18 @@ io.on('connection', function(socket){
   });
 
   socket.on('left', function(value){
-    value = stopMoving(socket.state, value)
     socket.left = value;
   });
 
   socket.on('right', function(value){
-    value = stopMoving(socket.state, value)
     socket.right = value;
   });
 
   socket.on('up', function(value){
-    value = stopMoving(socket.state, value)
     socket.up = value;
   });
 
   socket.on('down', function(value){
-    value = stopMoving(socket.state, value)
     socket.down = value;
   });
 
@@ -202,15 +197,3 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
-
-
-function stopMoving(state, value) {
-  row = floor(state.y / 32)
-  column = floor(state.x / 32)
-  if (maze[row][column] === 1) {
-    return 0
-  }
-  else {
-    return value
-  }
-}
