1dc93f03c807094b09502653fac167e453fd928c
Emily Barcroft <emily.barcroft@metaswitch.com>
2019-05-10 11:19:35 +0100

    .y

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ setInterval(function(){
             let state2 = connected[key2].state;
             if (state2.type === "ghost") {
               if (Math.abs(state.x - state2.x) < 32) {
-                if (Math.abs(state.y -state2.x) < 32) {
+                if (Math.abs(state.y -state2.y) < 32) {
                   console.log("crashed")
                   state.x = Math.floor(Math.random() * WIDTH);
                   state.y = Math.floor(Math.random() * HEIGHT);
