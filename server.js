ae18068dedf8f3e30be82e469d1a7e7cdb60f9ba
Bilen Ahmet <games.pelagic@gmail.com>
2019-05-10 04:17:46 +0100

    Allowing Heroku to set the port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -141,6 +143,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
